01d51a37577cbb9442813831c6043a0048309f0f
Markus Opolka <opolkams@iis.fraunhofer.de>
2017-10-21 09:21:26 +0200

    Add variables for database config
    - Somebody told me those weren't required anymore, someboby lied

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,14 +21,36 @@ class vision_intranet (
   String $mysql_monitoring_password = hiera('intranet::database::monitoring_password'),
   String $mysql_root_password       = hiera('intranet::database::root_password'),
 
+  String $mysql_adressen_database = hiera('intranet::database::adressen::name'),
+  String $mysql_adressen_user     = hiera('intranet::database::adressen::user'),
+  String $mysql_adressen_password = hiera('intranet::database::adressen::password'),
+
+  String $mysql_projekte_database = hiera('intranet::database::projekte::name'),
+  String $mysql_projekte_user     = hiera('intranet::database::projekte::user'),
+  String $mysql_projekte_password = hiera('intranet::database::projekte::password'),
+
+  String $mysql_voruntersuchungen_database = hiera('intranet::database::voruntersuchungen::name'),
+  String $mysql_voruntersuchungen_user     = hiera('intranet::database::voruntersuchungen::user'),
+  String $mysql_voruntersuchungen_password = hiera('intranet::database::voruntersuchungen::password'),
+
+  String $mysql_presse_database = hiera('intranet::database::presse::name'),
+  String $mysql_presse_user     = hiera('intranet::database::presse::user'),
+  String $mysql_presse_password = hiera('intranet::database::presse::password'),
+
 ) {
 
   contain vision_intranet::beanstalk
   contain vision_intranet::docker
   contain vision_intranet::database
 
-  file { ['/opt/intranet', '/opt/intranet/storage']:
+  file {
+    [
+      '/opt/intranet',
+      '/opt/intranet/storage'
+    ]:
     ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
   }
 
 
