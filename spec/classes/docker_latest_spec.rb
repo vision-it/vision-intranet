1a0ab78771690d3ed30dd11680817937725b25ce
Markus Opolka <opolkams@iis.fraunhofer.de>
2019-03-26 11:07:39 +0100

    Update Fixtures

--- spec/classes/docker_latest_spec.rb
+++ spec/classes/docker_latest_spec.rb
@@ -7,10 +7,14 @@ describe 'vision_intranet::docker' do
       let(:facts) do
         facts
       end
+
+      let(:pre_condition) { 'include vision_docker' }
+
       # Default check to see if manifest compiles
       context 'compile' do
         it { is_expected.to compile.with_all_deps }
       end
+
       context 'contains' do
         it { is_expected.to contain_docker__image('intranet').with_image_tag('latest') }
       end
--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -9,10 +9,14 @@ describe 'vision_intranet::docker' do
           intranet_tag: 'foobar'
         )
       end
+
+      let(:pre_condition) { 'include vision_docker' }
+
       # Default check to see if manifest compiles
       context 'compile' do
         it { is_expected.to compile.with_all_deps }
       end
+
       context 'contains' do
         it { is_expected.to contain_docker__image('intranet').with_image_tag('foobar') }
       end
